d2325be12afdb143ae72f84b8aa92aa87ae5b0d6
ChristopheAseglio <christophe.aseglio@buildwise.be>
2024-01-15 11:04:55 +0100

    gpio_input_test.py reading ok

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # StrainSensePi
 Internship Project for my Bachelor in IT about Strain sensors and Rpi
+
+Projet d'innovation pour la surveillance du retrait du béton : Intégration de Raspberry Pi avec PCBs et modules Adafruit pour mesurer les jauges de contrainte. 
+Collecte, envoi et analyse des données en temps réel sur le cloud. 
+Conçu pour améliorer la précision des essais de retrait du béton et faciliter la consultation des données.
---
+++ gpio_input_test.py
@@ -0,0 +1,34 @@
+import Adafruit_ADS1x15
+import time
+
+# Créer une instance ADS1115
+adc = Adafruit_ADS1x15.ADS1115(busnum=1)
+
+# Choisi un gain approprié
+GAIN = 2/3
+
+def read_differential_strain_gauge():
+    # Lit la tension entre le canal 0 et 1
+    value = adc.read_adc_difference(0, gain=GAIN)
+    # Converti la valeur en tension
+    voltage = value * (4.096 / 32767) * GAIN
+    return voltage
+
+def get_average_voltage(num_samples=10):
+    total_voltage = 0
+    for _ in range(num_samples):
+        total_voltage += read_differential_strain_gauge()
+        time.sleep(0.1)
+    return total_voltage / num_samples
+
+if __name__ == '__main__':
+    initial_voltage = get_average_voltage()
+    print(f"Tension initiale de la jauge de contrainte : {initial_voltage}")
+    try:
+        while True:
+            current_voltage = get_average_voltage()
+            voltage_change = current_voltage - initial_voltage
+            print(f"Tension actuelle : {current_voltage}, Changement par rapport à l'initial : {voltage_change}")
+            time.sleep(1)  # Mise à jour toutes les secondes
+    except KeyboardInterrupt:
+        print("Programme arrêté par l'utilisateur.")
